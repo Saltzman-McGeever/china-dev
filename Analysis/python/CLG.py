a02d1918190058d9c04066562cd45b93a7802fd8
ljsaltzman <levisaltzman@gmail.com>
2025-12-25 15:49:26 -0500

    fix directories

--- Analysis/python/CLG.py
+++ Analysis/python/CLG.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 # Get the directory where this script is located, create a Path object for the Excel file
 script_dir = Path(__file__).parent
-excel_path = script_dir / "AidDatas_CLG_Global_Dataset_v1.0.xlsx"
+excel_path = script_dir.parent / "AidDatas_CLG_Global_Dataset_v1.0.xlsx"
 
 clg = pd.read_excel(excel_path, sheet_name="CLG-Global 1.0_Records")
 
